f72023d505e43b6187e51ba61d0da351aae62a70
ignas traskevicius <ignas.traskevicius@gmail.com>
2013-09-22 18:57:41 +0300

    bookin system initial hours should be unbooked. Refactored

---
+++ src/main/java/lt/ignas/test/Booker.java
@@ -0,0 +1,17 @@
+package lt.ignas.test;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ignas
+ * Date: 9/22/13
+ * Time: 6:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Booker {
+    public List<Integer> getBookedHours() {
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/lt/ignas/test/BookingSystemTest.java
@@ -0,0 +1,32 @@
+package lt.ignas.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ignas
+ * Date: 9/22/13
+ * Time: 6:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Test
+public class BookingSystemTest {
+
+    private Booker booker;
+
+    @BeforeMethod
+    public void setUp() {
+        booker = new Booker();
+    }
+
+    // should return no hours when nobady has booked yet
+    public void shouldReturnNoBookedHoursWhenNobodyBookedYet() {
+        assertEquals(booker.getBookedHours(), Collections.emptyList());
+    }
+}
