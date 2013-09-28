52ffd2cb98684540b4943a15dfbc5df39e0cc236
ignas traskevicius <ignas.traskevicius@gmail.com>
2013-09-28 09:10:38 +0300

    test class created

---
+++ src/test/java/lt/ignas/classroombooking/ReservationPlaceTest.java
@@ -0,0 +1,14 @@
+package lt.ignas.classroombooking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ignas
+ * Date: 9/28/13
+ * Time: 9:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReservationPlaceTest {
+
+    //should be no classrooms to list initially
+
+}
